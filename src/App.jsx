2967928b890bc651dcb07bb90d07283cda8b74a2
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-20 08:06:02 +0530

    structure the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
+import "./App.css";
+import { CurrencyInput } from "./components/CurrencyInput";
 
 function App() {
-
-  return (
-    <>
-      <h1>Currency Converter</h1>
-    </>
-  )
+    return (
+        <>
+            <div className="container">
+                <header>
+                    <h1>Currency Converter</h1>
+                </header>
+                <main>
+                    <CurrencyInput label="amount" />
+                    <div className="swap">
+                        <button>
+                            <i class="ri-arrow-up-down-line"></i>
+                        </button>
+                    </div>
+                    <CurrencyInput label="converted amount" />
+                    <button className="convert">
+                        Convert {} to {}
+                    </button>
+                </main>
+                <footer>
+                  <p className="mini-text">Indicative Exchange Rate</p>
+                  <p>1 USD = 1.36 SGD</p>
+                </footer>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
