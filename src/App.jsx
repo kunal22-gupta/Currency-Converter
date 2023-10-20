e08aff7690711bd76248bcc4e666f36795068a1f
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-20 15:06:51 +0530

    add currency converting logic to elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import { CurrencyInput } from "./components/CurrencyInput";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
+    const [from, setFrom] = useState("usd");
+    const [to, setTo] = useState("inr");
+
+    const [amount, setAmount] = useState(0);
+    const [convertedAmount, setConvertedAmount] = useState(0);
+
+    const exchangeRates = useCurrencyInfo(from);
+
+    const currencyOptions = Object.keys(exchangeRates);
+
+    const swap = () => {
+        setFrom(to);
+        setTo(from);
+        setConvertedAmount((amount / exchangeRates[to]).toFixed(4));
+    };
+
+    const convert = () => {
+        setConvertedAmount((exchangeRates[to] * amount).toFixed(4));
+    };
+
     return (
         <>
             <div className="container">
@@ -9,20 +31,45 @@ function App() {
                     <h1>Currency Converter</h1>
                 </header>
                 <main>
-                    <CurrencyInput label="amount" />
-                    <div className="swap">
-                        <button>
-                            <i class="ri-arrow-up-down-line"></i>
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            convert();
+                        }}
+                    >
+                        <CurrencyInput
+                            label="amount"
+                            amount={amount}
+                            setAmount={setAmount}
+                            currency={from}
+                            setCurrency={setFrom}
+                            currencyOptions={currencyOptions}
+                        />
+                        <div className="swap">
+                            <button type="button" onClick={swap}>
+                                <i className="ri-arrow-up-down-line"></i>
+                            </button>
+                        </div>
+                        <CurrencyInput
+                            label="converted amount"
+                            amount={convertedAmount}
+                            setAmount={setConvertedAmount}
+                            currency={to}
+                            setCurrency={setTo}
+                            currencyOptions={currencyOptions}
+                            disabled
+                        />
+                        <button className="convert" type="submit">
+                            Convert {from.toUpperCase()} to {to.toUpperCase()}
                         </button>
-                    </div>
-                    <CurrencyInput label="converted amount" />
-                    <button className="convert">
-                        Convert {} to {}
-                    </button>
+                    </form>
                 </main>
                 <footer>
-                  <p className="mini-text">Indicative Exchange Rate</p>
-                  <p>1 USD = 1.36 SGD</p>
+                    <p className="mini-text">Indicative Exchange Rate</p>
+                    <p>
+                        1 {from.toUpperCase()} = {exchangeRates[to]}{" "}
+                        {to.toUpperCase()}
+                    </p>
                 </footer>
             </div>
         </>
--- src/components/CurrencyInput.jsx
+++ src/components/CurrencyInput.jsx
@@ -1,26 +1,47 @@
 import React, { useId } from "react";
 import "../css/CurrencyInput.css";
-export function CurrencyInput({ label }) {
+export function CurrencyInput({
+    label,
+    currencyOptions = [],
+    disabled = false,
+    amount,
+    setAmount,
+    currency = "usd",
+    setCurrency,
+}) {
     const amountId = useId();
     const currencyId = useId();
 
     return (
         <div className="CurrencyInput">
             <div className="currency">
-                <label htmlFor={currencyId} className="mini-text">Currency</label>
-                <select id={currencyId}>
-                    <option value="usd">usd</option>
-                    <option value="inr">inr</option>
-                    <option value="yen">yen</option>
-                    <option value="hey">Hey</option>
+                <label htmlFor={currencyId} className="mini-text">
+                    Currency
+                </label>
+                <select
+                    id={currencyId}
+                    value={currency}
+                    onChange={(e) => setCurrency(e.target.value)}
+                >
+                    {currencyOptions.map((currency) => (
+                        <option key={currency} value={currency}>
+                            {currency}
+                        </option>
+                    ))}
                 </select>
             </div>
             <div className="amount">
-                <label htmlFor={amountId} className="mini-text">{label}</label>
+                <label htmlFor={amountId} className="mini-text">
+                    {label}
+                </label>
                 <input
                     type="number"
                     id={amountId}
                     min="0"
+                    value={amount}
+                    placeholder="Amount"
+                    onChange={(e) => setAmount(Number(e.target.value))}
+                    disabled={disabled}
                 />
             </div>
         </div>
